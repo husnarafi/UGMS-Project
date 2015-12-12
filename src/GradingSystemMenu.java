9c9d011a4f28a9337ed60be8ee8c841a658ac5b3
Husna Rafi <husnarafi@Husnas-MacBook-Pro.local>
2015-12-13 07:23:37 +0800

    case 7 completed

--- src/GradingSystemMenu.java
+++ src/GradingSystemMenu.java
@@ -103,93 +103,168 @@ public class GradingSystemMenu {
 			case 2:
 				System.out.print("Please input the student ID:");
 				String stdIdForGrade = scanner.next();
-				
-					Student student = querymanager.findStudentByStudentId(stdIdForGrade, studentBag);
-					if (student == null){
-						System.out.print("No such student exists:");
 
-					}else {
-						//get courses from student
-						for (Course course : student.getCourses()){
-							System.out.println("Course Code : " + course.getCourseCode() + " Course Grade : " + course.getGrade());
-						}
-						System.out.println("GPA : " + student.getGpa());
+				Student student = querymanager.findStudentByStudentId(
+						stdIdForGrade, studentBag);
+				if (student == null) {
+					System.out.print("No such student exists:");
+
+				} else {
+					// get courses from student
+					for (Course course : student.getCourses()) {
+						System.out.println("Course Code : "
+								+ course.getCourseCode() + " Course Grade : "
+								+ course.getGrade());
 					}
-					
+					System.out.println("GPA : " + student.getGpa());
+				}
+
 				break;
-				
+
 			case 3:
 				Student[] stdArray = convertBagToArray(studentBag);
 				Merge.sort(stdArray);
-				
+
 				System.out.println("Student ID\t\tStudent Name\t\tGPA");
-				System.out.println("----------------\t--------------\t--------------");
-				
+				System.out
+						.println("----------------\t--------------\t--------------");
+
 				for (int i = 0; i < stdArray.length; i++) {
-					System.out.println(stdArray[i].getId() + "\t" + stdArray[i].getSurname() + "," + stdArray[i].getGivenname() +"\t"+ stdArray[i].getGpa());
+					System.out.println(stdArray[i].getId() + "\t"
+							+ stdArray[i].getSurname() + ","
+							+ stdArray[i].getGivenname() + "\t"
+							+ stdArray[i].getGpa());
 				}
-				
+
 				break;
 			case 4:
+
+				System.out.print("Please input the Student Surname: ");
+				String studentSurname = scanner.next();
+
+				Bag<Student> stdWithSurname = querymanager
+						.findStudentsBySurname(studentSurname, studentBag);
+
+				System.out.println(stdWithSurname.size()
+						+ " record(s) found in UGMS");
+				for (Student std : stdWithSurname) {
+					System.out.println(std.getGivenname() + " "
+							+ std.getSurname() + " " + std.getId() + " ");
+					for (Course course : std.getCourses()) {
+						System.out.println(course.getCourseCode() + " "
+								+ course.getGrade());
+
+					}
+					System.out.print("GPA:" + std.getGpa());
+
+				}
+
 				break;
 			case 5:
 				break;
 			case 6:
 				break;
 			case 7:
+				// Please choose the course code first: LP002
+				// After the course code is chosen (input by the user), a sample
+				// report is shown below:
+				// Course Code: LP002
+				// Credit: 4
+				// Number of Students: 8
+				// Name ID Score Grade
+				// -------------------- ------------------ ----- -----
+				// 6
+				// Chan, Henry 1109853A-I011-0224 86 AChoi,
+				// Randy 1109853A-I011-0018 60 C
+				// Ip, John 1109853A-I011-0456 66 C+
+				// Lau, Mary 1109853U-I011-0002 75 B
+				// Lee, Vito 1109853J-I011-0116 85 ALeung,
+				// Terry 1109853P-I011-0955 80 B+
+				// Ngai, Raymond 1109853Z-I011-0066 89 A
+				// Tung, Tracy 1109853Q-I011-0016 51 D
+				//
+				System.out.print("Please choose the course code first:");
+				String courseCode = scanner.next();
+
+				Bag<Student> students = querymanager.findStudentsByCourse(
+						courseCode, studentBag);
+				boolean onlyRunFirstTime = true;
+
+				for (Student std : students) {
+
+					float score = 0;
+					String grade = null;
+
+					for (Course course : std.getCourses()) {
+						if (onlyRunFirstTime) {
+							onlyRunFirstTime = false;
+							System.out.println("Course Code:"
+									+ course.getCourseCode());
+							System.out.println("Credit:"
+									+ course.getCourseCredit());
+							System.out.println("Number of Students:"
+									+ course.getNumberOfStudents());
+						}
+ 
+						score = course.getScore();
+						grade = course.getGrade();
+
+					}
+					System.out.println("Name\t" + std.getSurname() + ","
+							+ std.getGivenname() + "\tID\t" + std.getId() + "\tScore\t"
+							+ score + "\tGrade\t" + grade);
+				}
+
+				// System.out.println("Credit: ")
+
 				break;
 			case 8:
 				break;
 			case 9:
-				
-				//Get course code from user
-				//initialize num and denometor
+
+				// Get course code from user
+				// initialize num and denometor
 				// check this course code in loop
-				// count total score in nume 
+				// count total score in nume
 				// count total values in denominator
-				
-				
+
 				// student have courses..getstudentCourses
 				// for each course, average, highest and lowest
-				//iterate loop
-				System.out.println("Course Code:" );
+				// iterate loop
+				System.out.println("Course Code:");
 				System.out.println("Course Credit: ");
-				
-				String courseCode = scanner.next();
-				String courseCredit = scanner.next();
-				
-				
+
+				// String courseCode = scanner.next();
+				// String courseCredit = scanner.next();
+
 				float average = 0;
-				float average1= 0;
-				float average2 =0;
+				float average1 = 0;
+				float average2 = 0;
 				float average3 = 0;
-				
-				
-				for (Student std3 : studentBag){
-					for (Course course: std3.getCourses()){
-						
-						if (course.getCourseCode().equals("MA113")){
-							//average=course.getScore() +average;
-							
-							System.out.println("Course Code: " + courseCode);
-							System.out.println("Credit: " + courseCredit);
-							
+
+				for (Student std3 : studentBag) {
+					for (Course course : std3.getCourses()) {
+
+						if (course.getCourseCode().equals("MA113")) {
+							// average=course.getScore() +average;
+
+							// System.out.println("Course Code: " + courseCode);
+							// System.out.println("Credit: " + courseCredit);
+
 						}
-						
-						if (course.getCourseCode().equals("MA114")){
-							average =course.getScore() +average;
-						
+
+						if (course.getCourseCode().equals("MA114")) {
+							average = course.getScore() + average;
+
 						}
-						
-						if (course.getCourseCode().equals("MA115")){
+
+						if (course.getCourseCode().equals("MA115")) {
 							average = course.getScore() + average;
 						}
 					}
-					
-					
-					
+
 				}
-				
+
 				break;
 
 			default:
@@ -202,8 +277,7 @@ public class GradingSystemMenu {
 		}
 
 	}
-	
-	
+
 	private static Student[] convertBagToArray(Bag<Student> studentBag) {
 		int length = studentBag.size();
 		Student[] students = new Student[length];
--- src/QueryManager.java
+++ src/QueryManager.java
@@ -8,29 +8,65 @@ public class QueryManager {
 		// else no students id exists
 
 		for (Student std : studentBag) {
-			if (studentId.equals(std.getId())) { 
+			if (studentId.equals(std.getId())) {
 				return std.getGpa();
 			}
 		}
-		
+
 		return -1;
 	}
-	public Student findStudentByStudentId(String studentId, Bag<Student> studentBag){
-		//we have course code, grade and gpa in our studentbag
-		// recalling student function 
+
+	public Student findStudentByStudentId(String studentId,
+			Bag<Student> studentBag) {
+		// we have course code, grade and gpa in our studentbag
+		// recalling student function
 		// iterate loop
-		//check the student id in std bag
+		// check the student id in std bag
 		// if yes, then coursecode, grade and gpa
-		for (Student std : studentBag){
-			if (studentId.equals(std.getId())){
+		for (Student std : studentBag) {
+			if (studentId.equals(std.getId())) {
 				return std;
-			
+
 			}
-		
+
 		}
 		return null;
 	}
 
+	public Bag<Student> findStudentsBySurname(String surname,
+			Bag<Student> studentBag) {
+		Bag<Student> students = new Bag<Student>();
+
+		for (Student std : studentBag) {
+			if (surname.equalsIgnoreCase(std.getSurname())) {
+				students.add(std);
+
+			}
+		}
+		return students;
+	}
+
+	public Bag<Student> findStudentsByCourse(String courseCode,
+			Bag<Student> studentBag) {
+		Bag<Student> students = new Bag<Student>();
+
+		for (Student std : studentBag) {
+			for (Course course : std.getCourses()) {
+				if (courseCode.equals(course.getCourseCode())) {
+					Student s = new Student();
+					s.setGivenname(std.getGivenname());
+					s.setSurname(std.getSurname());
+					s.setId(std.getId());
+					s.setGpa(std.getGpa());
+					Bag<Course> c = new Bag<Course>();
+					c.add(course);
+					s.setCourses(c);
+					students.add(s);
+
+				}
+
+			}
+		}
+		return students;
+	}
 }
-
-
